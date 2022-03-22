87cf199f20d89e3f007d1305357fe11e79487f6e
Paul Kim <paulkim3151@gmail.com>
2022-03-22 11:17:00 +0000

    First video upload

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,14 +8,14 @@ import {
 	getChangePassword,
 	postChangePassword,
 } from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware, uploadFiles} from "../middlewares";
+import { protectorMiddleware, publicOnlyMiddleware, avatarUpload} from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.route("/edit")
 	.all(protectorMiddleware)
 	.get(getEdit)
-	.post(uploadFiles.single("avatar"),postEdit);
+	.post(avatarUpload.single("avatar"),postEdit);
 userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
