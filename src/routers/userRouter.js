bfa7e7937a6aaf985e8d1b73749e2f5e5fc18c1e
Paul Kim <paulkim3151@gmail.com>
2022-03-22 11:41:04 +0000

    Add user profile page

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -16,9 +16,12 @@ userRouter.route("/edit")
 	.all(protectorMiddleware)
 	.get(getEdit)
 	.post(avatarUpload.single("avatar"),postEdit);
-userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
+userRouter.route("/change-password")
+	.all(protectorMiddleware)
+	.get(getChangePassword)
+	.post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
-userRouter.get(":id", see);
+userRouter.get("/:id([0-9a-f]{24})", see);
 
 export default userRouter;
